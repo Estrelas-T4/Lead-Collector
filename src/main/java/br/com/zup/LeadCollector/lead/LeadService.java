cca517d2279aa25b95f22a8b621779c75c017afe
Vinícius <vinicius.raymundo@zup.com.br>
2021-10-29 14:28:28 -0300

    Implementar lista de mailing

--- src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
@@ -1,7 +1,12 @@
 package br.com.zup.LeadCollector.lead;
 
+import br.com.zup.LeadCollector.lead.dtos.LeadDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LeadService {
+    private List<LeadDTO> mailing = new ArrayList<>();
 }
