6399642f280d9332824d7d2a2550512b14560653
Vinícius <vinicius.raymundo@zup.com.br>
2021-10-29 14:24:49 -0300

    Criar classe DTO para lead

---
+++ src/main/java/br/com/zup/LeadCollector/lead/dtos/LeadDTO.java
@@ -0,0 +1,47 @@
+package br.com.zup.LeadCollector.lead.dtos;
+
+import br.com.zup.LeadCollector.produto.dtos.ProdutoDTO;
+
+import java.util.List;
+
+public class LeadDTO {
+    private String email;
+    private String nome;
+    private String telefone;
+    private List<ProdutoDTO> produtos;
+
+    public LeadDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<ProdutoDTO> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<ProdutoDTO> produtos) {
+        this.produtos = produtos;
+    }
+}
